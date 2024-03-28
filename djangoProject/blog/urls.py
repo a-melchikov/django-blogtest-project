be0e99e23fc9a2dbcb52754362bcd8fefde25bb3
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-29 00:42:04 +0400

    Added the ability to create a post for users who have logged in

--- djangoProject/blog/urls.py
+++ djangoProject/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AboutPageView, BlogDetailView, BlogList
+from .views import AboutPageView, BlogDetailView, BlogList, create_post
 
 urlpatterns = [
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path('create_post/', create_post, name='create_post'),
     path("", BlogList.as_view(), name="home"),
 ]
--- djangoProject/blog/views.py
+++ djangoProject/blog/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView, DetailView, TemplateView
 from .models import Post
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from blog.forms import PostForm
 
 
 class BlogList(ListView):
@@ -14,3 +17,16 @@ class BlogDetailView(DetailView):
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+
+@login_required  # Декоратор, чтобы требовать аутентификацию пользователя
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')  # Перенаправляем пользователя на главную страницу после создания поста
+    else:
+        form = PostForm()
+    return render(request, 'create_post.html', {'form': form})

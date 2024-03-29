126523017e320cf14a5e9d2179c9ee576cf8bb11
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-29 22:01:58 +0400

    Fixed it. No profile was created when creating the user

--- djangoProject/authentication/forms.py
+++ djangoProject/authentication/forms.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
+from .models import CustomUser, Profile
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ["username", "email", "age"]  # Поля для формы регистрации
+        fields = ["username", "email", "age"]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.save()
+        profile = Profile.objects.create(user=user)  # Создание профиля
+        return user

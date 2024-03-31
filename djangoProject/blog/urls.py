3f1a9323678081848c9d15f094cecc5598677e66
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-31 23:47:51 +0400

    Inbox and send_message pages have been created

--- djangoProject/blog/urls.py
+++ djangoProject/blog/urls.py
@@ -9,16 +9,20 @@ from .views import (
     edit_post,
     edit_profile,
     my_posts,
+    send_message,
+    inbox,
 )
 
 urlpatterns = [
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("post/delete/<int:pk>/", PostDeleteView.as_view(), name="delete_post"),
-    path('post/edit/<int:pk>/', edit_post, name='edit_post'),
+    path("post/edit/<int:pk>/", edit_post, name="edit_post"),
     path("about/", AboutPageView.as_view(), name="about"),
     path("create_post/", create_post, name="create_post"),
     path("profile/", ProfilePageView.as_view(), name="profile"),
     path("profile/edit/", edit_profile, name="edit_profile"),
     path("my_posts/", my_posts, name="my_posts"),
     path("catalog/", BlogList.as_view(), name="home"),
+    path("send_message/", send_message, name="send_message"),
+    path("inbox/", inbox, name="inbox"),
 ]

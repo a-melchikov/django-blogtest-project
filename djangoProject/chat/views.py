b952af3413df3194622228f0bf4d37aab16fe4c6
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-04-01 22:21:40 +0400

    Adding a template and a view

--- djangoProject/chat/views.py
+++ djangoProject/chat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def chat_view(request, room_name):
+    return render(
+        request, "chat/chat.html", {"room_name": room_name, "user": request.user}
+    )
--- djangoProject/djangoProject/asgi.py
+++ djangoProject/djangoProject/asgi.py
@@ -1,16 +1,22 @@
 import os
 from django.core.asgi import get_asgi_application
+from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from chat.consumers import ChatConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Добавляем WebSocket-маршрутизатор для обработки запросов WebSocket
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            # URL-шаблоны WebSocket-обработчиков
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        # Добавляем WebSocket-маршрутизатор для обработки запросов WebSocket
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                [
+                    path("chat/<str:room_name>/", ChatConsumer.as_asgi()),
+                ]
+            )
+        ),
+    }
+)

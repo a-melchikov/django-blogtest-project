8e91e08978e08fb6e745237beb7a23585c743a63
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-04-06 22:03:56 +0400

    Fixed a bug that did not go to the user's edit page. verbose_name is also assigned for attributes

--- djangoProject/authentication/models.py
+++ djangoProject/authentication/models.py
@@ -3,13 +3,17 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_of_birth = models.DateField(null=True, blank=True)
-    country = models.CharField(max_length=50, blank=True)
-    city = models.CharField(max_length=50, blank=True)
-    registration_date = models.DateField(auto_now_add=True)
-    bio = models.TextField(max_length=500, blank=True)
-    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    date_of_birth = models.DateField(null=True, blank=True, verbose_name="Дата рождения")
+    country = models.CharField(max_length=50, blank=True, verbose_name="Страна")
+    city = models.CharField(max_length=50, blank=True, verbose_name="Город")
+    registration_date = models.DateField(auto_now_add=True, verbose_name="Дата регистрации")
+    bio = models.TextField(max_length=500, blank=True, verbose_name="О себе")
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True, verbose_name="Аватар")
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
 
     def __str__(self):
         return self.user.username

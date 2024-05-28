decc4a44324d30e449ed1c22ea52205920c80a65
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-05-28 22:34:00 +0400

    The names in the admin panel have been changed

--- djangoProject/messaging/apps.py
+++ djangoProject/messaging/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class MessagingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'messaging'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "messaging"
+    verbose_name = "Сообщения"
